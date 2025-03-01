81d31d4ac8fa2c017844a09da8ebe08136e22267
dba.philip <dba.philip>
2025-03-01 23:55:01 +0100

    Cart Creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
 
@@ -15,4 +18,13 @@ class Product < ApplicationRecord
       errors.add(:image, "must be a GIF, JPG, WEBP or PNG image")
     end
   end
+
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, "Line Items present")
+        throw :abort
+      end
+    end
 end
